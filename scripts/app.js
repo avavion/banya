9f212b5efb02e149402fc91e4e79c09033a04a8d
avavion <avavionmvm@gmail.com>
2020-12-11 02:25:00 +0300

    Rewrite slider and added new styles

--- scripts/app.js
+++ scripts/app.js
@@ -18,46 +18,50 @@ const scrollOff = () => {
 
 // Слайдер
 
-const slider = (sliderSelector, sliderHeight = 500) => {
-  let SliderHeight = sliderHeight;
+// Слайдер
+const slider = (sliderSelector, sliderItemsSelector, nextButtonSelector, prevButtonSelector) => {
+  const slider = document.querySelector(sliderSelector),
+    sliderItems = slider.querySelectorAll(sliderItemsSelector),
+    nextButton = document.querySelector(nextButtonSelector),
+    prevButton = document.querySelector(prevButtonSelector),
+    sliderWrapper = document.querySelector(".slider__wrapper");
+  let currentSlide = 0;
+  let sliderWidth = sliderWrapper.offsetWidth;
+  slider.style.width = `${sliderWidth}px`;
+  slider.style.height = `600px`;
 
-  const Slider = document.querySelector(sliderSelector);
-  Slider.style.height = `${SliderHeight}px`;
-  const list = Slider.querySelector(".slider-list");
-  list.style.height = `${SliderHeight}px`;
-  const items = list.querySelectorAll(".slider-item");
-  const dots = Slider.querySelectorAll(".slider-dot");
-
-  let slideWidth = 0;
-
-  console.log(slideWidth);
-
-  items.forEach((item) => {
-    item.style.height = `${SliderHeight - 50}px`;
-    slideWidth += item.offsetWidth;
-    item.setAttribute("data-width", slideWidth);
-    item.style.transform = `translateX(${slideWidth}px)`;
-    console.log(`translateX(${slideWidth}px)`);
+  sliderItems.forEach((item, index) => {
+    item.style.width = `100%`;
+    item.style.height = `600px`;
   });
 
-  dots.forEach((dot, index) => {
-    let nextSlide = index + 1;
-    let prevSlide = dots.length - 1;
-    dot.addEventListener("click", () => {
-      if (prevSlide < 0) {
-        prevSlide = dots.length;
-        console.log(prevSlide);
-      }
-      if (nextSlide >= dots.length) {
-        nextSlide = 0;
-      }
-      console.log(items[prevSlide]);
-      items[prevSlide].style.transform = `translateX(-${items[index].getAttribute("data-width")}px)`;
-      items[index].style.transform = `translateX(0px)`;
-      let prepareNextSlide = items[nextSlide].getAttribute("data-width") - items[index].getAttribute("data-width");
-      items[nextSlide].style.transform = `translateX(${prepareNextSlide}px)`;
-    });
+  const slide = () => {
+    for (let index = 0; index < sliderItems.length; index++) {
+      sliderItems[index].classList.remove("active");
+    }
+    sliderItems[currentSlide].classList.add("active");
+  };
+
+  nextButton.addEventListener("click", () => {
+    if (currentSlide + 1 == sliderItems.length) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+    slide();
   });
+
+  prevButton.addEventListener("click", () => {
+    if (currentSlide - 1 == -1) {
+      currentSlide = sliderItems.length - 1;
+    } else {
+      currentSlide--;
+    }
+    slide();
+  });
+
+  // Первичная иницилизация слайдера, чтобы первый элемент был активный сразу
+  slide();
 };
 
 // Модальное окно
@@ -92,5 +96,5 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   scrollButton.addEventListener("click", scrollUp);
   modalWindow(".js-modal", ".js-openModal");
-  slider(".slider");
+  slider(".slider", ".slider-item", ".slider-prev-arrow", ".slider-next-arrow");
 });
